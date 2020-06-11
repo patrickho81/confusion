b9006b34386854b1acd83ed557ad4079d994ba1a
Patrick <patrickho81@gmail.com>
2020-06-11 16:58:59 -0700

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import Dishdetail from "./DishdetailComponent";
 import { View, Platform } from "react-native";
-
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
 
 const MenuNavigator = createStackNavigator(
   {
@@ -24,6 +25,45 @@ const MenuNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+    }),
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#D1C4E9",
+  }
+);
+
 class Main extends Component {
   render() {
     return (
@@ -34,7 +74,7 @@ class Main extends Component {
           paddingTop: Platform.OS === "ios" ? 0 : 1,
         }}
       >
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     );
   }
